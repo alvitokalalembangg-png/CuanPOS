ac1a7b1016561bb71381a44f928a0929ed61c9ec
Ariya11M <aryamisiu@gmail.com>
2026-07-20 12:29:33 +0700

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,15 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    // Retrofit & JSON Parsing
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // OkHttp for logging API responses in Logcat
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // Coroutines for asynchronous network calls
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
